a61dbe3d54fda40d98019f55f2509e906edab7d2
Xabier <xabier@baxabe.com>
2023-06-06 18:05:08 +0200

    GetPriceByDataRangeAndProductAndBrandService interface defined. GetPriceByDataRangeAndProductAndBrandInput interface defined. GetPriceByDataRangeAndProductAndBrandOutput interface defined. Minor refactoring PriceEntity.

---
+++ src/main/java/com/baxabe/inditex/service/GetPriceByDataRangeAndProductAndBrandService.java
@@ -0,0 +1,12 @@
+package com.baxabe.inditex.service;
+
+public interface GetPriceByDataRangeAndProductAndBrandService {
+
+    GetPriceByDataRangeAndProductAndBrandInput getInput();
+
+    void setInput(GetPriceByDataRangeAndProductAndBrandInput input);
+
+    GetPriceByDataRangeAndProductAndBrandOutput getOutput();
+
+    void setOutput(GetPriceByDataRangeAndProductAndBrandOutput output);
+}
